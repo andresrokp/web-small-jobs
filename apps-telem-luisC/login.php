c443822429c2c48615da40acd5d9bddbd2b893f6
Andres Roca P <afrokap@gmail.com>
2022-05-30 19:19:33 -0500

    init php approach | include form | admin page

---
+++ apps-telem-luisC/login.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.84.0">
+    <title>Heroes · Bootstrap v5.0</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">   
+
+  </head>
+<body>
+    <div class="container col-xl-10 col-xxl-8 px-4">
+    <div class="row align-items-center g-lg-5 py-2">
+    <div class="col-lg-7 text-center text-lg-start">
+        <h2 class="display-5 fw-bold lh-1 mb-3">GESTIÓN DE ACTIVOS EN LA TIENDA</h2>
+        <p class="col-lg-10 fs-4">Esta es un app sencilla para la asignatura de Aplicaciones Telemáticas de la UNAD. La app consiste en esta página de inicio que contiene un banner, un inicio de sesión y la lista de elemtos en la tienda</p>
+        <small>Por: Luis Ceballos</small>
+    </div>
+    <div class="col-md-10 mx-auto col-lg-5">
+        <form class="p-4 p-md-5 border rounded-3 bg-light" autocomplete="off" action="index.php" method="post">
+        <div class="form-floating mb-3">
+            <input class="form-control" id="floatingInput" placeholder="name@example.com" autocomplete="off">
+            <label for="floatingInput">Usuario</label>
+        </div>
+        <div class="form-floating mb-3">
+            <input type="password" class="form-control" id="floatingPassword" placeholder="Password" autocomplete="off">
+            <label for="floatingPassword">Contraseña</label>
+        </div>
+        <div class="checkbox mb-3">
+            <label>
+            <input type="checkbox" value="remember-me"> Remember me
+            </label>
+        </div>
+        <button class="w-100 btn btn-lg btn-primary" type="submit">Iniciar Sesión</button>
+        </form>
+    </div>
+    </div>
+    </div>
+</body>
